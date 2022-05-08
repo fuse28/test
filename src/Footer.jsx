4729341c67d85d3ec71d4cfce1d8b7716ba3c97e
fuse28 <mixfuse92@gmail.com>
2022-05-08 19:01:41 +0530

    first commit

---
+++ src/Footer.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <div className="numbers">
+      <button className="btn1">1</button>
+      <button className="btn2">2</button>
+      <button className="btn3">3</button>
+      <button className="btn4">4</button>
+      <button className="btn5">5</button>
+    </div>
+  );
+}
+export default Footer;
